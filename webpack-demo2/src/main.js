1a2c8f1be3d2a4b79ce00fac37f18754c029bcdf
hfl <690328661@qq.com>
2019-01-06 10:40:58 +0800

    first commit

---
+++ webpack-demo2/src/main.js
@@ -0,0 +1,7 @@
+//项目的js入口文件
+console.log('OK');
+
+import './css/index.css'
+import './css/index.scss'
+//注意：如果要通过路径的形式，去引用node_modules 中相关的文件，可以直接省略 路径前面的 
+//node_modules 这一层目录，直接写包的名称，然后后面跟上具体的文件路径
