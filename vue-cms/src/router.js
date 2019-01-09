4e6deffb6b4d1f8765a4208b0d7bf449f556ab5b
hfl <690328661@qq.com>
2019-01-09 14:48:18 +0800

    07项目-完成tabbar路由链接的改造和路由高

--- vue-cms/src/router.js
+++ vue-cms/src/router.js
@@ -5,8 +5,9 @@ import VueRouter from 'vue-router'
 //3.创建一个viewrouter
 var router = new VueRouter({
     routes:[
-       
-    ]
+        
+    ],
+    linkActiveClass: 'mui-active' //覆盖默认的路由高亮的类。默认的类叫做 router-link-active
 })
 
 //吧路由对象暴露出去
