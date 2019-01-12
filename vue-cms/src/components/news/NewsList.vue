c71e713bde694ef46e1c7e1fc05f19b9dd436a6e
hfl <690328661@qq.com>
2019-01-12 15:47:34 +0800

    02项目-绘制新闻资讯列表页面布局

--- vue-cms/src/components/news/NewsList.vue
+++ vue-cms/src/components/news/NewsList.vue
@@ -1,10 +1,62 @@
 <template>
     <div>
-        <h3>新闻列表页面</h3>
+        <ul class="mui-table-view">
+            <li class="mui-table-view-cell mui-media">
+                <a href="javascript:;">
+                    <img class="mui-media-object mui-pull-left" src="../../images/swap01.jpg">
+                    <div class="mui-media-body">
+                        <h1>幸福</h1>
+                        <p class='mui-ellipsis'>
+                            <span>发布时间: 2018-12-12 12:12:12</span>
+                            <span>点击: 0次</span>
+                        </p>
+                    </div>
+                </a>
+            </li>
+            <li class="mui-table-view-cell mui-media">
+                <a href="javascript:;">
+                    <img class="mui-media-object mui-pull-left" src="../../images/swap02.jpg">
+                    <div class="mui-media-body">
+                        <h1>木屋</h1>
+                        <p class='mui-ellipsis'>
+                            <span>发布时间: 2018-12-12 12:12:12</span>
+                            <span>点击: 0次</span>
+                        </p>
+                    </div>
+                </a>
+            </li>
+            <li class="mui-table-view-cell mui-media">
+                <a href="javascript:;">
+                    <img class="mui-media-object mui-pull-left" src="../../images/swap01.jpg">
+                    <div class="mui-media-body">
+                        <h1>CBD</h1>
+                        <p class='mui-ellipsis'>
+                            <span>发布时间: 2018-12-12 12:12:12</span>
+                            <span>点击: 0次</span>
+                        </p>
+                    </div>
+                </a>
+            </li>
+
+        </ul>
     </div>
 </template>
 
 <script></script>
 
 <style lang="scss" scoped>
+    .mui-table-view{
+        li{
+            h1{
+                font-size: 14px;
+            }
+            .mui-ellipsis{
+                font-size: 12px;
+                color: #226aff;
+                display: flex;
+                justify-content: space-between;
+            }
+        }
+    }
+
 </style>
